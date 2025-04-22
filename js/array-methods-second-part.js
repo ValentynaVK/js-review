41bc95141b2aab1e4e0da5d8ea6b44d365c0c75c
ValentynaVK <valia2526.vk@gmail.com>
2025-04-22 15:14:57 +0300

    arrays-hw-all

--- js/array-methods-second-part.js
+++ js/array-methods-second-part.js
@@ -70,12 +70,87 @@
 // - Скласти список унікальних гравців, які брали участь у турнірі.
 // - Створити масив з тими гравцями, хто хоча б один раз вигравав.
 
-const games = [
-  ["Alice", "Bob", "white"], // Alice виграла
-  ["Charlie", "David", "black"], // David виграв
-  ["Eve", "Frank", "draw"], // Нічия
-  ["Alice", "Charlie", "white"], // Alice виграла
-  ["Bob", "Eve", "black"], // Eve виграла
-  ["David", "Frank", null], // гра була скасована
-  ["Alice", "Eve", "white"], // Alice виграла
-];
+// const games = [
+//   ["Alice", "Bob", "white"], // Alice виграла
+//   ["Charlie", "David", "black"], // David виграв
+//   ["Eve", "Frank", "draw"], // Нічия
+//   ["Alice", "Charlie", "white"], // Alice виграла
+//   ["Bob", "Eve", "black"], // Eve виграла
+//   ["David", "Frank", null], // гра була скасована
+//   ["Alice", "Eve", "white"], // Alice виграла
+// ];
+
+// const draw = games.some(([, , result]) => result === "draw");
+
+// const allGamesPlayed = games.every(([, , result]) => result !== null);
+
+// let whiteWins = 0;
+// let blackWins = 0;
+
+// const winners = new Set();
+// const players = new Set();
+
+// for (const [white, black, result] of games) {
+//   winners.add(white);
+//   players.add(black);
+
+//   if (result === "white") {
+//     whiteWins++;
+//     winners.add(white);
+//   } else if (result === "black") {
+//     blackWins++;
+//     winners.add(black);
+//   }
+// }
+// const uniquePlayers = Array.from(players);
+// const playersWithWins = Array.from(winners);
+
+// console.log(draw);
+// console.log(allGamesPlayed);
+// console.log(whiteWins);
+// console.log(blackWins);
+// console.log(winners);
+// console.log(players);
+// console.log(uniquePlayers);
+// console.log(playersWithWins);
+
+//=========================================================================================
+
+//5. Є дані про результати бігових змагань у вигляді одного масиву. Кожен елемент масиву — це рядок, що містить ім'я учасника та його час у секундах, розділені двокрапкою.  Потрібно обробити ці дані за допомогою методів масивів.
+// - Розбити рядки на окремі елементи - отримати масив, кожен елемент якого є масивом з двох елементів: ім'я та числове значення часу.
+// - Знайти найшвидший час.
+// - Перевірити, чи всі учасники показали результат менше 15 секунд.
+// - Перевірити, чи хтось пробіг швидше 11 секунд.
+// - Сформувати фінальний рядок ТОП-3 учасників. Використати slice і join, щоб створити рядок типу *"1. Grace (10.5s), 2. Charlie (11.8s), 3. Alice (12.4s)"*.
+
+// const results = [
+//   "Hank:16.3",
+//   "David:14.1",
+//   "Alice:12.4",
+//   "Bob:15.2",
+//   "Charlie:11.8",
+//   "Eve:13.5",
+//   "Frank:12.9",
+//   "Grace:10.5",
+// ];
+
+// const parsedResults = results.map((item) => {
+//   const [name, time] = item.split(":");
+//   return [name, parseFloat(time)];
+// });
+
+// const bestTime = Math.min(...parsedResults.map(([_, time]) => time));
+// const allUnderFifteen = parsedResults.every(([_, time]) => time < 15);
+// const underEleven = parsedResults.some(([_, time]) => time < 11);
+
+// const topThree = parsedResults
+//   .sort((a, b) => a[1] - b[1])
+//   .slice(0, 3)
+//   .map(([name, time], index) => `${index + 1}. ${name} (${time}s)`)
+//   .join(", ");
+
+// console.log(parsedResults);
+// console.log(bestTime);
+// console.log(allUnderFifteen);
+// console.log(underEleven);
+// console.log(topThree);
